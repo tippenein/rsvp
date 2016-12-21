2e643f4d81b7b600bae289522c2255cb62fd01a2
brady.ouren <brady.ouren@gmail.com>
2016-12-20 20:50:46 -0800

    fix specs

--- rsvp-server/tests/Main.hs
+++ rsvp-server/tests/Main.hs
@@ -5,6 +5,8 @@ module Main
 import Protolude
 
 import Control.Monad.Log (Severity(..))
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
 import Servant.QuickCheck
        ((<%>), createContainsValidLocation, defaultArgs, not500,
         notLongerThan, serverSatisfies,
@@ -12,7 +14,7 @@ import Servant.QuickCheck
 import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Hspec (Spec, it, testSpec)
 
-import Rsvp.API (api)
+import Rsvp.API (api, ContactInfo(..), Event(..), Rsvp(..), User(..))
 import Rsvp.Server (server)
 
 main :: IO ()
@@ -23,6 +25,20 @@ tests = do
   specs <- testSpec "quickcheck tests" spec
   pure $ testGroup "Rsvp.Server" [specs]
 
+instance Arbitrary ContactInfo where
+  arbitrary = Phone <$> arbitrary
+
+instance Arbitrary Event where
+  arbitrary = Event <$> pos <*> arbitrary <*> arbitrary
+     where pos = getPositive <$> arbitrary
+
+instance Arbitrary Rsvp where
+  arbitrary = Rsvp <$> pos <*> arbitrary <*> arbitrary
+     where pos = getPositive <$> arbitrary
+
+instance Arbitrary User where
+  arbitrary = User <$> arbitrary <*> arbitrary <*> arbitrary
+
 spec :: Spec
 spec =
   it "follows best practices" $
@@ -32,5 +48,8 @@ spec =
        api
        burl
        defaultArgs
-       (not500 <%> createContainsValidLocation <%> notLongerThan 100000000 <%> unauthorizedContainsWWWAuthenticate <%>
+       (not500 <%>
+        createContainsValidLocation <%>
+        notLongerThan 100000000 <%>
+        unauthorizedContainsWWWAuthenticate <%>
         mempty)
