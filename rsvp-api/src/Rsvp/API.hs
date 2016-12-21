3ca896d7b6cfc94d2711b3ef5557b3bdcee83de9
brady.ouren <brady.ouren@gmail.com>
2016-12-20 19:07:40 -0800

    add basic starting types for Event, Rsvp, User

--- rsvp-api/src/Rsvp/API.hs
+++ rsvp-api/src/Rsvp/API.hs
@@ -5,13 +5,7 @@
 {-# LANGUAGE TypeOperators #-}
 
 -- | API definition for rsvp.
-module Rsvp.API
-  ( API
-  , api
-  , RootPage(..)
-  , User(User)
-  , Users(..)
-  ) where
+module Rsvp.API where
 
 import Protolude
 
@@ -19,48 +13,99 @@ import Data.Aeson
        (FromJSON(..), ToJSON(..), Value(..), object, (.=), (.:))
 import Data.Aeson.Types (typeMismatch)
 import qualified NeatInterpolation as NI
-import Servant.API ((:>), (:<|>)(..), Get, JSON, MimeRender(..))
+import Servant.API ((:>), ReqBody,(:<|>)(..), Get, Post, JSON, MimeRender(..))
 
 import Rsvp.API.Internal (HTML)
 
--- | rsvp API definition.
-type API = Get '[HTML] RootPage :<|> "users" :> Get '[JSON] Users
+type API = Get '[HTML] RootPage :<|>
+  ListUsers :<|>
+  ListRsvps :<|>
+  ListEvents :<|>
+  CreateEvent
 
--- | Value-level representation of API.
+type ListUsers = "users" :> Get '[JSON] UserResponse
+type ListRsvps = "rsvps" :> Get '[JSON] RsvpResponse
+type ListEvents = "events" :> Get '[JSON] EventResponse
+type CreateEvent =
+  "events"
+  :> ReqBody '[JSON] Event
+  :> Post '[JSON] EventResponse
+
 api :: Proxy API
 api = Proxy
 
--- | Example object. Replace this with something relevant to your app.
 data User = User
   { _userId :: Int
   , _userFirstName :: Text
   , _userLastName :: Text
   } deriving (Eq, Show, Generic)
 
-instance FromJSON User
-
-instance ToJSON User
-
--- | Represents a list of users.
---
--- We have a newtype so we can be sure to return a JSON object.
-newtype Users =
-  Users [User]
+data ContactInfo
+  = Phone Text
+  | Email Text
   deriving (Eq, Show, Generic)
 
-instance FromJSON Users where
-  parseJSON (Object v) = Users <$> v .: "users"
+data Rsvp
+  = Rsvp
+  { _rsvp_event_id :: Int
+  , _rsvp_name :: Text
+  , _rsvp_contact :: ContactInfo
+  } deriving (Eq, Show, Generic)
+
+data Event = Event
+  { _event_creator_id :: Int -- User id
+  , _event_creator_contact :: ContactInfo
+  , _event_name:: Text
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON User
+instance ToJSON User
+
+instance FromJSON Rsvp
+instance ToJSON Rsvp
+
+instance FromJSON Event
+instance ToJSON Event
+
+instance FromJSON ContactInfo
+instance ToJSON ContactInfo
+
+newtype UserResponse =
+  UserResponse [User]
+  deriving (Eq, Show, Generic)
+
+instance FromJSON UserResponse where
+  parseJSON (Object v) = UserResponse <$> v .: "users"
   parseJSON x = typeMismatch "Users" x
 
-instance ToJSON Users where
-  toJSON (Users users) = object ["users" .= toJSON users]
+instance ToJSON UserResponse where
+  toJSON (UserResponse users) = object ["users" .= toJSON users]
 
--- | Represents the root page of the service.
+newtype RsvpResponse =
+  RsvpResponse [Rsvp]
+  deriving (Eq, Show, Generic)
+
+instance FromJSON RsvpResponse where
+  parseJSON (Object v) = RsvpResponse <$> v .: "rsvps"
+  parseJSON x = typeMismatch "Rsvps" x
+
+instance ToJSON RsvpResponse where
+  toJSON (RsvpResponse rsvps) = object ["rsvps" .= toJSON rsvps]
+
+newtype EventResponse =
+  EventResponse [Event]
+  deriving (Eq, Show, Generic)
+
+instance FromJSON EventResponse where
+  parseJSON (Object v) = EventResponse <$> v .: "events"
+  parseJSON x = typeMismatch "Events" x
+
+instance ToJSON EventResponse where
+  toJSON (EventResponse events) = object ["events" .= toJSON events]
+
 data RootPage =
   RootPage
 
--- | Very simple root HTML page. Replace this with your own simple page that
--- describes your API to other developers and sysadmins.
 instance MimeRender HTML RootPage where
   mimeRender _ _ =
     toS
@@ -72,6 +117,7 @@ instance MimeRender HTML RootPage where
          <h1>rsvp</h1>
          <ul>
          <li><a href="/users">users</a></li>
+         <li><a href="/events">events</a></li>
          <li><a href="/metrics"><code>/metrics</code></a></li>
          </ul>
          <p>
--- rsvp-server/src/Rsvp/Server/Handlers.hs
+++ rsvp-server/src/Rsvp/Server/Handlers.hs
@@ -6,10 +6,6 @@ module Rsvp.Server.Handlers
   ( server
   ) where
 
--- XXX: jml doesn't like the name "Handlers" for this, and isn't sure that it
--- should be in a submodule of Project.Server. Perhaps the code in
--- Project.Server (which is command-line processing, setting up logs &
--- monitoring, starting the HTTP server) should be in a different module.
 import Protolude hiding (Handler)
 
 import Control.Monad.Except (ExceptT(..))
@@ -17,14 +13,19 @@ import Control.Monad.Log (Severity, logInfo)
 import Servant (ServantErr, Server, (:<|>)(..), (:~>)(..), enter)
 import Text.PrettyPrint.Leijen.Text (Doc, Pretty, text)
 
-import Rsvp.API (API, RootPage(..), User(..), Users(..))
+import Rsvp.API
 import qualified Rsvp.Server.Logging as Log
 
 -- | rsvp API implementation.
 server :: Severity -> Server API
 server logLevel = enter (toHandler logLevel) handlers
   where
-    handlers = pure RootPage :<|> users
+    handlers =
+      pure RootPage :<|>
+      users :<|>
+      rsvps :<|>
+      events :<|>
+      createEvent
 
 -- | Our custom handler type.
 type Handler msg = ExceptT ServantErr (Log.LogM msg IO)
@@ -43,8 +44,24 @@ toHandler logLevel = Nat toHandler'
       => Handler msg a -> ExceptT ServantErr IO a
     toHandler' = ExceptT . Log.withLogging logLevel . runExceptT
 
--- | Example endpoint.
-users :: Handler Doc Users
+users :: Handler Doc UserResponse
 users = do
   logInfo (text "Example of logging")
-  pure (Users [User 1 "Isaac" "Newton", User 2 "Albert" "Einstein"])
+  pure (UserResponse [User 1 "Isaac" "Newton", User 2 "Albert" "Einstein"])
+
+events :: Handler Doc EventResponse
+events = do
+  logInfo (text "showing all events")
+  pure (EventResponse [Event 1 (Phone "1112223434") "event brawl", Event 2 (Email "derp@derp.com") "whatever event"])
+
+
+rsvps :: Handler Doc RsvpResponse
+rsvps = do
+  logInfo (text "showing all Rsvps")
+  pure (RsvpResponse [])
+
+createEvent :: Event -> Handler Doc EventResponse
+createEvent event = do
+  putText $ show event
+  logInfo (text "creating new event ")
+  pure (EventResponse [])
