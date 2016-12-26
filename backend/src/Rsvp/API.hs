00b619a081a192c48f5d7b58a918a86f757e5a78
brady.ouren <brady.ouren@gmail.com>
2016-12-26 09:24:51 -0800

    serve static files

--- backend/src/Rsvp/API.hs
+++ backend/src/Rsvp/API.hs
@@ -10,24 +10,32 @@ module Rsvp.API where
 import           Protolude
 
 import           Data.Int (Int64)
--- import qualified Data.Text as T
+-- import Data.Aeson (ToJSON(..))
+import Data.Aeson.Types (Options, fieldLabelModifier, defaultOptions)
+import qualified Data.Text as T
 -- import           Database.Persist.Types (Entity(..))
 import qualified NeatInterpolation as NI
--- import qualified Prelude
+import qualified Prelude
 import           Servant.API
 
 import           Rsvp.Server.Models
 import           Rsvp.Response
 import           Rsvp.API.Internal (HTML)
 
-type API =
+type API = RsvpAPI :<|> Raw
+type RsvpAPI =
   Get '[HTML] RootPage :<|>
   "users" :> Get '[JSON] UserResponse :<|>
   "rsvps" :> Get '[JSON] RsvpResponse :<|>
-  "events" :> Capture "id" Int64 :> Get '[JSON] Event :<|>
-  "events" :> QueryParam "name" Text :> Get '[JSON] EventResponse :<|>
-  CreateEvent -- :<|>
-  -- Raw
+  GetEvent:<|>
+  ListEvents :<|>
+  CreateEvent
+
+type GetEvent =
+  "events" :> Capture "id" Int64 :> Get '[JSON] Event
+
+type ListEvents =
+  "events" :> QueryParam "name" Text :> Get '[JSON] EventResponse
 
 type CreateEvent =
   "events"
@@ -37,52 +45,10 @@ type CreateEvent =
 api :: Proxy API
 api = Proxy
 
--- data User = User
---   { _user_name :: Text
---   , _user_contact :: ContactInfo
---   } deriving (Eq, Show, Generic)
+-- json -----------------------------------------------
 
--- data ContactInfo
---   = Phone Text
---   | Email Text
---   deriving (Eq, Show, Generic)
-
--- data Rsvp
---   = Rsvp
---   { _rsvp_event_id :: Int64
---   , _rsvp_name :: Text
---   , _rsvp_contact :: ContactInfo
---   } deriving (Eq, Show, Generic)
-
--- data Event = Event
---   { _event_creator_id :: Int64 -- User id
---   , _event_creator_contact :: ContactInfo
---   , _event_name:: Text
---   } deriving (Eq, Show, Generic)
-
--- -- json -----------------------------------------------
--- instance FromJSON User where
---   parseJSON = genericParseJSON defaultOptions {
---                 fieldLabelModifier = dropIdentifier }
--- instance ToJSON User where
---   toJSON = genericToJSON defaultOptions {
---              fieldLabelModifier = dropIdentifier }
-
--- instance FromJSON Rsvp where
---   parseJSON = genericParseJSON defaultOptions {
---                 fieldLabelModifier = dropIdentifier }
--- instance ToJSON Rsvp where
---   toJSON = genericToJSON defaultOptions {
---              fieldLabelModifier = dropIdentifier }
-
--- instance FromJSON Event where
---   parseJSON = genericParseJSON defaultOptions {
---                 fieldLabelModifier = dropIdentifier }
--- instance ToJSON Event where
---   toJSON = genericToJSON aesonDef
-
--- aesonDef :: Options
--- aesonDef = defaultOptions { fieldLabelModifier = dropIdentifier }
+aesonDef :: Options
+aesonDef = defaultOptions { fieldLabelModifier = dropIdentifier }
 
 -- instance ToJSON (Entity Event) where
 --   toJSON (Entity pid (e@Event{..})) = toJSON e <$> object [ "id" .= pid ]
@@ -99,8 +65,8 @@ api = Proxy
 --   toJSON (Phone t) = object [ "phone" .= t]
 --   toJSON (Email t) = object [ "email" .= t]
 
--- dropIdentifier :: Prelude.String -> Prelude.String
--- dropIdentifier = T.unpack . T.intercalate "_" . drop 2 . T.splitOn "_" . T.pack
+dropIdentifier :: Prelude.String -> Prelude.String
+dropIdentifier = T.unpack . T.intercalate "_" . drop 2 . T.splitOn "_" . T.pack
 
 data RootPage =
   RootPage
@@ -109,17 +75,21 @@ instance MimeRender HTML RootPage where
   mimeRender _ _ =
     toS
       [NI.text|
-         <!doctype html>
-         <html>
-         <head><title>RSVP</title></head>
-         <body>
-         <h1>RSVP</h1>
-         <ul>
-         <li><a href="/users">users</a></li>
-         <li><a href="/events">events</a></li>
-         <li><a href="/rsvps">rsvps</a></li>
-         <li><a href="/metrics"><code>/metrics</code></a></li>
-         </ul>
-         </body>
-         <html>
-         |]
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <script language="javascript" src="rts.js"></script>
+            <script language="javascript" src="lib.js"></script>
+            <script language="javascript" src="out.js"></script>
+          </head>
+          <body>
+            <ul>
+            <li><a href="/users">users</a></li>
+            <li><a href="/events">events</a></li>
+            <li><a href="/rsvps">rsvps</a></li>
+            <li><a href="/metrics"><code>/metrics</code></a></li>
+            </ul>
+          </body>
+          <script language="javascript" src="runmain.js" defer></script>
+        </html>
+      |]
--- backend/src/Rsvp/Server/Handlers.hs
+++ backend/src/Rsvp/Server/Handlers.hs
@@ -11,20 +11,24 @@ import           Protolude hiding (get, (%), from)
 import           Control.Monad.Except (ExceptT(..))
 import           Control.Monad.Log (logInfo)
 import           Control.Monad.Reader (ReaderT, runReaderT)
-import           Database.Persist.Sql (toSqlKey, selectList)
 import           Database.Esqueleto
-import           Servant (err404, ServantErr, Server, (:<|>)(..), (:~>)(..), enter)
+import           Database.Persist.Sql (toSqlKey, selectList)
+import           Network.Wai (Application)
+import           Servant (serveDirectory, err404, ServantErr, Server, (:<|>)(..), (:~>)(..), enter)
 import           Text.PrettyPrint.Leijen.Text (Doc, Pretty, text)
 
-import qualified Rsvp.Server.Config as Config
 import           Rsvp.API
 import           Rsvp.Response
-import           Rsvp.Server.Models
+import qualified Rsvp.Server.Config as Config
 import qualified Rsvp.Server.Logging as Log
+import           Rsvp.Server.Models
+
+files :: Application
+files = serveDirectory "static"
 
 -- | rsvp API implementation.
 server :: Config.Config -> Server API
-server config = enter (toHandler config) handlers
+server config = enter (toHandler config) handlers :<|> files
   where
     handlers =
       pure RootPage :<|>
@@ -33,7 +37,6 @@ server config = enter (toHandler config) handlers
       getEvent :<|>
       events :<|>
       createEvent
-      -- files
 
 -- | Our custom handler type.
 type Handler msg = ReaderT Config.Config (ExceptT ServantErr (Log.LogM msg IO))
@@ -42,7 +45,6 @@ type Handler msg = ReaderT Config.Config (ExceptT ServantErr (Log.LogM msg IO))
 -- | Translate our custom monad into a Servant handler.
 --
 -- See http://haskell-servant.readthedocs.io/en/stable/tutorial/Server.html#using-another-monad-for-your-handlers
--- for the details.
 toHandler
   :: Pretty msg
   => Config.Config -> Handler msg :~> ExceptT ServantErr IO
@@ -55,29 +57,29 @@ convertToHandler
   -> ExceptT ServantErr IO a
 convertToHandler cfg = ExceptT . Log.withLogging (Config.logLevel cfg) . runExceptT . flip runReaderT cfg
 
--- convertApp :: Config.Config -> Config.App :~> ExceptT ServantErr IO
--- convertApp cfg = Nat (flip runReaderT cfg . Config.runApp)
-
 users :: Handler Doc UserResponse
 users = do
-  logInfo (text "Example of logging")
-  -- pure (UserResponse [User "Isaac" (Phone "1234445556"), User "Albert" (Email "whatever@whatever.com")])
-  pure (UserResponse [User "Isaac" "1234445556", User "Albert" "whatever@whatever.com"])
+  us <- runDb $ selectList [] []
+  pure (UserResponse $ fmap entityVal us)
 
 getEvent :: Int64 -> Handler Doc Event
 getEvent id = do
-  -- logInfo (text $ "getting event " <> show id)
-  -- pure (Event (toSqlKey id) "event brawl" "1112223434")
   record <- runDb $ get (toSqlKey id)
   case record of
     Nothing -> throwError err404
     Just a -> pure a
 
+createEvent :: Event -> Handler Doc EventCreateResponse
+createEvent event = do
+  putText $ show event
+  _event_id <- runDb $ insert event
+  logInfo (text $ "created new event " <> show event)
+  pure (EventCreateResponse (Right "success"))
+
 events :: Maybe Text -> Handler Doc EventResponse
 events mname =
   case mname of
     Nothing -> do
-      logInfo (text "showing all events")
       es <- runDb $ selectList [] []
       pure (EventResponse $ fmap entityVal es)
     Just name -> do
@@ -89,12 +91,5 @@ events mname =
 
 rsvps :: Handler Doc RsvpResponse
 rsvps = do
-  logInfo (text "showing all Rsvps")
-  pure (RsvpResponse [])
-
-createEvent :: Event -> Handler Doc EventCreateResponse
-createEvent event = do
-  putText $ show event
-  _event_id <- runDb $ insert event
-  logInfo (text "created new event ")
-  pure (EventCreateResponse (Right "success"))
+  es <- runDb $ selectList [] []
+  pure (RsvpResponse $ fmap entityVal es)
