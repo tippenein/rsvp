a474c7f9a4b5a3c4d160d8f342d42da0b5c23e9a
brady.ouren <brady.ouren@gmail.com>
2016-12-30 14:08:04 -0800

    fix circle build

--- backend/tests/Main.hs
+++ backend/tests/Main.hs
@@ -78,11 +78,3 @@ unitTests :: Spec
 unitTests =
   it "passes" $
     True `shouldBe` True
-
---   [ testCase "List comparison (different length)" $
---       [1, 2, 3] `compare` [1,2] @?= GT
-
---   -- the following test does not hold
---   , testCase "List comparison (same length)" $
---       [1, 2, 3] `compare` [1,2,2] @?= LT
---   ]
